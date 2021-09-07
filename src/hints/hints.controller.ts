d9fc465286de97a4f082d54cc3cabcc9a17af10c
Marco Rezende <rezende@espressolabs.com.br>
2021-09-07 20:55:15 -0300

    hints.controller: "Crud" adicionado

--- src/hints/hints.controller.ts
+++ src/hints/hints.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { HintsService } from './hints.service';
 import { Hint } from './entities/hint.entity';
-import { CrudController } from '@nestjsx/crud';
+import { Crud, CrudController, CrudRequest } from '@nestjsx/crud';
+import { GENRES } from 'src/genre/models/genre.model';
+import { getManager } from 'typeorm';
 
+@Crud({
+  model: {
+    type: Hint,
+  },
+  query: {
+    join: {
+      genres: { eager: true },
+    },
+  },
+  params: {
+    id: {
+      field: 'id',
+      type: 'string',
+      primary: true,
+    },
+  },
+})
 @Controller('hints')
 export class HintsController implements CrudController<Hint> {
   constructor(public service: HintsService) {}
