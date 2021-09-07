e349b524867337eb33852a4c8a3cbf02bded52ba
Marco Rezende <rezende@espressolabs.com.br>
2021-09-07 08:28:26 -0300

    database configurada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configValidationSchema } from './config/config.schema';
 import { HintsModule } from './hints/hints.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HintsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    HintsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
