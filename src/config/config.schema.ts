c20ba4df55fe68c3193b6d9079c2612720558b85
Marco Rezende <rezende@espressolabs.com.br>
2021-09-07 15:40:41 -0300

    config.schema: schema atualizado

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -6,10 +6,9 @@ export const configValidationSchema = Joi.object({
    */
   PORT: Joi.number().default(3000),
 
-  STAGE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
+  DB_PORT: Joi.number().default(3306).required(),
   DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
+  DB_PASSWORD: Joi.string().default('').optional(),
   DB_DATABASE: Joi.string().required(),
 });
