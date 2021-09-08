3f8a6e5b40d83a910c36c22d18f562a0c365bbe3
Marco Rezende <rezende@espressolabs.com.br>
2021-09-07 21:00:05 -0300

    genre.controller: métodos removidos

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
-import { getManager } from 'typeorm';
 import { Genre } from './entities/genre.entity';
 import { GenreService } from './genre.service';
-import { GENRES } from './models/genre.model';
 
 @Crud({
   model: {
@@ -13,17 +11,4 @@ import { GENRES } from './models/genre.model';
 @Controller('genre')
 export class GenreController implements CrudController<Genre> {
   constructor(public service: GenreService) {}
-
-  @Get('getOne')
-  async getOne() {
-    return await this.service.find({ relations: ['hint'] });
-  }
-
-  @Post('createOne')
-  async createOne() {
-    const hint = new Genre({ name: GENRES.ROMANCE });
-    const manager = getManager();
-
-    return await manager.save(hint);
-  }
 }
