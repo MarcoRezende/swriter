1ec8fd4e3935fdb2b3b7605c0c85bcf9109d6861
Marco Rezende <rezende@espressolabs.com.br>
2021-09-13 07:14:35 -0300

    hint.entity: relações substituídas por "category"

--- src/hints/entities/hint.entity.ts
+++ src/hints/entities/hint.entity.ts
@@ -1,28 +1,14 @@
-import { Genre } from 'src/genre/entities/genre.entity';
-import { Mood } from 'src/mood/entities/mood.entity';
-import { Sentence } from 'src/sentence/entities/sentence.entity';
+import { Category } from 'src/category/entities/category.entity';
 import { BaseEntity } from 'src/_common/base_entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity()
 export class Hint extends BaseEntity<Hint> {
-  @OneToMany(type => Genre, genre => genre.hint, {
+  @OneToMany(type => Category, genre => genre.hint, {
     onDelete: 'SET NULL',
     nullable: true,
   })
-  genres: Genre[];
-
-  @OneToMany(type => Genre, genre => genre.hint, {
-    onDelete: 'SET NULL',
-    nullable: true,
-  })
-  moods: Mood[];
-
-  @OneToMany(type => Genre, genre => genre.hint, {
-    onDelete: 'SET NULL',
-    nullable: true,
-  })
-  sentences: Sentence[];
+  categories: Category[];
 
   @Column({ nullable: false })
   tip: string;
