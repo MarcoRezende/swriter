f67e27b46c76d4ca954ef0908ffc31c6d4de3614
Marco Rezende <rezende@espressolabs.com.br>
2021-09-25 20:27:43 -0300

    hint.entity: adicionadas colunas "author", "book", "timesDrawn" e "bookmarked"

--- src/hints/entities/hint.entity.ts
+++ src/hints/entities/hint.entity.ts
@@ -1,6 +1,7 @@
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Category } from 'src/category/entities/category.entity';
 import { BaseEntity } from 'src/_common/base_entity';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { AfterLoad, Column, Entity, OneToMany } from 'typeorm';
 
 @Entity()
 export class Hint extends BaseEntity<Hint> {
@@ -12,4 +13,27 @@ export class Hint extends BaseEntity<Hint> {
 
   @Column({ nullable: false })
   tip: string;
+
+  @IsOptional()
+  @IsString()
+  @Column({ nullable: true })
+  author?: string;
+
+  @IsOptional()
+  @IsString()
+  @Column({ nullable: true })
+  book?: string;
+
+  @IsNumber()
+  @Column({ nullable: false, default: 0 })
+  timesDrawn?: number;
+
+  @IsBoolean()
+  @Column({ nullable: true, default: false })
+  bookmarked?: boolean;
+
+  @AfterLoad()
+  updateTimesDrawn() {
+    this.timesDrawn++;
+  }
 }
