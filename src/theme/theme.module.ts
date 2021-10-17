956aa6b3bddf237853700464e63c6ea97eab1404
Marco Rezende <rezende@espressolabs.com.br>
2021-10-17 00:02:52 -0300

    theme.module: módulo configurado

--- src/theme/theme.module.ts
+++ src/theme/theme.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Theme } from './entities/theme.entity';
+import { ThemeController } from './theme.controller';
+import { ThemeService } from './theme.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Theme])],
+  controllers: [ThemeController],
+  providers: [ThemeService],
+})
 export class ThemeModule {}
