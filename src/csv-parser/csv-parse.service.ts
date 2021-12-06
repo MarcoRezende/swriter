fd844bac22abfff53fcbaa22b6c86a5031196dfa
marco <marcorezende@outlook.com.br>
2021-12-05 21:27:09 -0300

    definido service para upload de arquivo CSV

---
+++ src/csv-parser/csv-parse.service.ts
@@ -0,0 +1,89 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { promises, createReadStream } from 'fs';
+import { parse } from 'csv-parse';
+import { EntityManager } from 'typeorm';
+
+interface Entity<T> {
+  new (...args: any): T;
+}
+
+interface Options {
+  insertIntoTable?: boolean;
+  delimiter?: string;
+}
+
+@Injectable()
+export class CsvParserService {
+  constructor() {}
+
+  async parseCsv<K = any>(
+    manager: EntityManager,
+    file: Express.Multer.File,
+    EntityTarget: Entity<K>,
+    options?: Options,
+  ): Promise<K[]> {
+    new EntityTarget();
+    return new Promise(resolve => {
+      const entities: K[] = [];
+      const stream = createReadStream(file.path);
+      let count = 0;
+      let headers = [];
+
+      stream
+        .pipe(parse({ delimiter: options.delimiter || ',' }))
+        .on('data', async (row: string[]) => {
+          if (count < 1) {
+            headers = row;
+          } else if (count > 1) {
+            entities.push(
+              new EntityTarget(
+                row.reduce((entity: K, rowProp: string, index) => {
+                  if (row.length > headers.length) {
+                    throw new BadRequestException(
+                      'Invalid row length',
+                      'SURPLUS_PROPERTY',
+                    );
+                  }
+
+                  entity[headers[index]] =
+                    rowProp.charAt(0).toUpperCase() + rowProp.slice(1);
+                  return entity;
+                }, {} as K),
+              ),
+            );
+          }
+          count++;
+        })
+        .on('end', () => {
+          promises.unlink(file.path); // remove o arquivo especificado
+
+          if (options?.insertIntoTable) {
+            this.insert<K>(manager, entities, EntityTarget, headers);
+          }
+
+          resolve(entities);
+        })
+        .on('error', err => {
+          throw new BadRequestException(err);
+        });
+    });
+  }
+
+  private async insert<K>(
+    manager: EntityManager,
+    values: K[],
+    EntityTarget: Entity<K>,
+    headers: string[],
+  ) {
+    await manager
+      .createQueryBuilder()
+      .insert()
+      .into(EntityTarget)
+      .values(values)
+      .orUpdate({
+        conflict_target: headers,
+        overwrite: headers,
+      })
+      .execute();
+  }
+}
