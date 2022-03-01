899cf2b3e0d5db8623c50fc7fa706fef17be8682
marco <marcorezende@outlook.com.br>
2022-02-28 21:50:39 -0300

    user: definido admin controller

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entities/user.entity';
+import { UserAdminController } from './user.admin.controller';
 import { UserAppController } from './user.app.controller';
 import { UsersService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserAppController],
+  controllers: [UserAppController, UserAdminController],
   providers: [UsersService],
   exports: [UsersService],
 })
